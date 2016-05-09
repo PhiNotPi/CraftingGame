9b10da0da4fbc4ff1dcd30b658dcf68c9f5c8b83
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-09 00:39:40 -0400

    Update Tag.java

--- src/Tag.java
+++ src/Tag.java
@@ -4,34 +4,40 @@ public class Tag {
   Boolean bool;
   Double min;
   Double max;
-  public Tag(String name){
+
+  public Tag(String name) {
     this.name = name;
   }
-  public Tag(String name, boolean bool){
+
+  public Tag(String name, boolean bool) {
     this.name = name;
     this.bool = bool;
   }
-  public Tag(String name, double val){
+
+  public Tag(String name, double val) {
     this.name = name;
     this.min = val;
     this.max = val;
   }
-  public Tag(String name, double min, double max){
+
+  public Tag(String name, double min, double max) {
     this.name = name;
     this.min = min;
     this.max = max;
   }
-  public String fieldName(){
+
+  public String fieldName() {
     return TagData.getFieldName(name);
   }
+
   public String toString() {
-    if(this.min != null){
-      return "["+name+":"+min+"]";
+    if (this.min != null) {
+      return "[" + name + ":" + min + "]";
     }
-    if(this.bool != null){
-      return "["+name+":"+bool+"]";
+    if (this.bool != null) {
+      return "[" + name + ":" + bool + "]";
     }
-      return "["+fieldName()+":"+name+"]";
+    return "[" + fieldName() + ":" + name + "]";
   }
 
 }
