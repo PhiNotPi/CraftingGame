f1d0abae97c811c0ef2019e8e18ddacfe9cf6cf2
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-10 23:04:57 -0400

    random TagSet generation

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,22 @@ public class Main {
 
   public static void main(String[] args) throws FileNotFoundException {
     Scanner in = new Scanner(new File("tags.dat"));
-    while(in.hasNextLine()){
+    while (in.hasNextLine()) {
       TagDataParser.tokenize(in.nextLine());
     }
     in.close();
     TagDataParser.parse();
     TagDataParser.build();
+    // TagDataParser.print();
+    System.out.println("Tag data:");
     TagData.printAll();
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println();
+      TagSet ts = TagData.randomTS();
+      ts.print();
+      ts.printAll();
+    }
   }
 
 }
