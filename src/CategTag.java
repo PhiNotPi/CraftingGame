533db843b76a47d99f1bafce825303f457450243
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-11 20:50:00 -0400

    tag comparison

--- src/CategTag.java
+++ src/CategTag.java
@@ -9,4 +9,30 @@ public class CategTag extends Tag {
   public String toString() {
     return "[" + fieldName + ":" + name + "]";
   }
+
+  @Override
+  public boolean isa(Tag tag) {
+    if (tag == null || !(tag instanceof CategTag)) {
+      return false;
+    }
+    CategTag other = (CategTag) tag;
+    if (this.fieldName.equals(other.fieldName)
+        && (this.name.equals(other.name) || TagData.getAllOptions(other.name)
+            .contains(name))) {
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || !(obj instanceof CategTag)) {
+      return false;
+    }
+    CategTag other = (CategTag) obj;
+    if (this.fieldName.equals(other.fieldName) && this.name.equals(other.name)) {
+      return true;
+    }
+    return false;
+  }
 }
