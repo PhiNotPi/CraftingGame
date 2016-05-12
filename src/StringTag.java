5a5b5b64203985bcb7fa675b610d6e8d9970f59a
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-11 20:56:53 -0400

    tag comparison

--- src/StringTag.java
+++ src/StringTag.java
@@ -9,4 +9,21 @@ public class StringTag extends Tag {
   public String toString() {
     return "[" + fieldName + ":" + name + "]";
   }
+
+  @Override
+  public boolean isa(Tag tag) {
+    return this.equals(tag);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || !(obj instanceof StringTag)) {
+      return false;
+    }
+    StringTag other = (StringTag) obj;
+    if (this.fieldName.equals(other.fieldName) && this.name == other.name) {
+      return true;
+    }
+    return false;
+  }
 }
