8c3fbc043c0877aa9fc5c1cc0626dc9eeab04488
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-11 20:57:25 -0400

    tag comparison

--- src/Tag.java
+++ src/Tag.java
@@ -3,6 +3,10 @@ public abstract class Tag {
 
   public abstract String toString();
 
+  public abstract boolean isa(Tag tag);
+
+  public abstract boolean equals(Object obj);
+
   public boolean isValid() {
     return TagData.isValid(this);
   }
