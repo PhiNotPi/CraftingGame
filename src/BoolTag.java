d61c38e5d5698eaf932e758662eb154d7b1d1c20
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-11 20:48:57 -0400

    Update BoolTag.java

--- src/BoolTag.java
+++ src/BoolTag.java
@@ -9,4 +9,21 @@ public class BoolTag extends Tag {
   public String toString() {
     return "[" + fieldName + ":" + val + "]";
   }
+
+  @Override
+  public boolean isa(Tag tag) {
+    return this.equals(tag);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || !(obj instanceof BoolTag)) {
+      return false;
+    }
+    BoolTag other = (BoolTag) obj;
+    if (this.fieldName.equals(other.fieldName) && this.val == other.val) {
+      return true;
+    }
+    return false;
+  }
 }
