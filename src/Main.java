84d9bed59c5af172da3749a0a37276b6dd361cf8
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-05-12 16:39:32 -0400

    random recipe generation and matching

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -19,21 +22,37 @@ public class Main {
     // TagData.setImplicit("name", new StringTag("name","whatever"));
     TagData.printAll();
     // System.out.println(new CategTag("granite").isa(new CategTag("stone")));
-    Ingredient ing = new Ingredient();
-    ing.pro = TagData.randomTS();
-    System.out.println(ing);
+    Recipe rec = new Recipe();
+    for (int i = 0; i < 3; i++) {
+      Ingredient ing = new Ingredient();
+      ing.pro = TagData.randomTS();
+      rec.ingredients.add(ing);
+    }
+    // ing = new Ingredient();
+    // ing.pro = TagData.randomTS();
+    // rec.ingredients.add(ing);
+    // ing = new Ingredient();
+    // ing.pro = TagData.randomTS();
+    // rec.ingredients.add(ing);
+
+    System.out.println(rec);
     TagSet ts = TagData.randomTS();
-    while (!ing.matches(ts)) {
-      System.out.println();
-      ts.print();
-      ts.printAll();
-      System.out.println(ing.matches(ts));
+    Set<TagSet> accum = new HashSet<TagSet>();
+    // accum.add(ing.pro);
+    while (Recipe.match(rec.ingredients, accum).size() != rec.ingredients
+        .size()) {
+      // System.out.println(Recipe.match(rec.ingredients, accum));
       ts = TagData.randomTS();
+      accum.add(ts);
+      // System.out.println();
+      // ts.print();
     }
     System.out.println();
-    ts.print();
-    ts.printAll();
-    System.out.println(ing.matches(ts));
+    Map<TagSet, Ingredient> res = Recipe.match(rec.ingredients, accum);
+    for (TagSet react : res.keySet()) {
+      System.out.println(react);
+      System.out.println("  " + res.get(react));
+    }
 
     // Ingredient ing2 = new Ingredient();
     // ing2.pro.add(new CategTag("stone"));
